a4dbdd9d662542a6f8ef44bf39cb1ffd840a09c5
mtgnk <mtgnk2020@gmail.com>
2020-07-28 21:31:37 +0900

    :tada: Initial Commit

---
+++ src/ts/index.ts
@@ -0,0 +1,8 @@
+// testコメント
+$('#app').append('<span>テスト</span>')
+$('#app').append('<span>テスト</span>')
+$('#app').append('<span>テスト</span>')
+
+$('#app').on('click',function(){
+    alert('test')
+})
